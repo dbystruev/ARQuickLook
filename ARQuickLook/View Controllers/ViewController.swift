ed61b6e51c5b89544c9539f1dc6dea70db18a38b
dbystruev <github@learnswift.ru>
2019-11-02 23:21:08 +0300

    Generated thumbnails

--- ARQuickLook/View Controllers/ViewController.swift
+++ ARQuickLook/View Controllers/ViewController.swift
@@ -6,28 +6,39 @@
 //  Copyright © 2019 Denis Bystruev. All rights reserved.
 //
 
-import QuickLookThumbnailing
 import UIKit
 
 class ViewController: UIViewController {
     
+    // MARK: - Outlets
     @IBOutlet var tableView: UITableView!
     
-    var usdzModels: [String] {
+    // MARK: - Computed Properties
+    var usdzModelNames: [String] {
         guard let resourceURL = Bundle.main.resourceURL else { return [] }
-        
         let usdzPath = resourceURL.appendingPathComponent("Assets.scnassets").path
         
-        guard let usdzFiles = try? FileManager.default.contentsOfDirectory(atPath: usdzPath) else {
-            return []
-        }
+        guard let usdzFiles = try? FileManager.default.contentsOfDirectory(atPath: usdzPath) else { return [] }
         
         return usdzFiles.filter { $0.hasSuffix(".usdz") }
     }
+    
+    // MARK: - Stored Properties
+    var usdzModels = [USDZModel]()
 
+    // MARK: - UIViewController Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(#line, #function, usdzModels)
+        
+        // Fill USDZ Models array with actual models data
+        usdzModelNames.forEach { name in
+            let fullName = "Assets.scnassets/\(name)"
+            usdzModels.append(USDZModel(named: fullName))
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            print(#line, #function, self.usdzModels)
+        }
     }
     
 }
