3c643d9fd8abfd5661810b071318483afc5824f8
dbystruev <github@learnswift.ru>
2019-11-02 22:29:26 +0300

    Listed files in Assets.scnassets

--- ARQuickLook/View Controllers/ViewController.swift
+++ ARQuickLook/View Controllers/ViewController.swift
@@ -6,15 +6,28 @@
 //  Copyright © 2019 Denis Bystruev. All rights reserved.
 //
 
+import QuickLookThumbnailing
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    var usdzModels: [String] {
+        guard let resourceURL = Bundle.main.resourceURL else { return [] }
+        
+        let usdzPath = resourceURL.appendingPathComponent("Assets.scnassets").path
+        
+        guard let usdzFiles = try? FileManager.default.contentsOfDirectory(atPath: usdzPath) else {
+            return []
+        }
+        
+        return usdzFiles.filter { $0.hasSuffix(".usdz") }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(#line, #function, usdzModels)
     }
-
-
+    
 }
-
